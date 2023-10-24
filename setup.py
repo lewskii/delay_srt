3c24ce6b512bee1630072d5a4e274877c3e84600
lewski <leevi.aaltonen@protonmail.com>
2023-10-24 07:16:40 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("./README.md") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name = "delay_srt",
+    version = "0.1.0",
+    description = "Delays the subtitles in a SubRip (.srt) file.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/lewskii/delay_srt",
+    author = "lewski",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Utilities"
+    ],
+    keywords = "delay subtitles subrip srt",
+    packages = setuptools.find_packages()
+)
