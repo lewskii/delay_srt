ca5eb1a9ead2fb40705716232b63fa0777cb0e5d
lewski <leevi.aaltonen@protonmail.com>
2023-10-24 04:02:13 +0300

    clean up main loop in delay_srt()

--- delay_srt/delay_srt.py
+++ delay_srt/delay_srt.py
@@ -13,25 +13,22 @@ def delay_srt(path: str, delay_ms: int):
 
     new_lines = []
     
-    find_next = False
+    waiting_for_next_subtitle = False
     for line in lines:
-        if find_next:
-            if not line.strip():
-                find_next = False
-            new_lines.append(line)
-            continue
-        
-        timestamps = re.findall(
-            r"(\d+):(\d+):(\d+),(\d+)",
-            line
-        )
-        if timestamps:
-            start, end = timestamps
-            start = delay_timestamp(start, delay_ms)
-            end = delay_timestamp(end, delay_ms)
-            new_lines.append(f"{start} --> {end}\n")
+        if waiting_for_next_subtitle and not line.strip():
+            waiting_for_next_subtitle = False
         else:
-            new_lines.append(line)
+            timestamps = re.findall(
+                r"(\d+):(\d+):(\d+),(\d+)",
+                line
+            )
+            if len(timestamps) == 2:
+                start = delay_timestamp(timestamps[0], delay_ms)
+                end = delay_timestamp(timestamps[1], delay_ms)
+                line = f"{start} --> {end}\n"
+                waiting_for_next_subtitle = True
+        
+        new_lines.append(line)
 
     with open(path, 'w') as file:
         file.writelines(new_lines)
